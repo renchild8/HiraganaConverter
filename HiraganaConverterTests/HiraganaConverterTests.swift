9265532b6fea829c81439c416785e2468c7af5da
rencild8 <rencild84@gmail.com>
2019-10-01 22:14:53 +0900

    Initial Commit

---
+++ HiraganaConverterTests/HiraganaConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  HiraganaConverterTests.swift
+//  HiraganaConverterTests
+//
+//  Created by 佐藤優 on 2019/10/01.
+//  Copyright © 2019 佐藤優. All rights reserved.
+//
+
+import XCTest
+@testable import HiraganaConverter
+
+class HiraganaConverterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
